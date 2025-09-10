b746ebf6ac188dfb9db84188171e6e599aee0f77
Manideep Kadarla <manideepkadarla23@gmail.com>
2025-09-09 21:25:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassActivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string sentence = Console.ReadLine();
+
+            int lettercount = 0;
+            foreach (char c in sentence)
+            {
+                if (char.IsLetter(c))
+                {
+                    lettercount++;
+                }
+            }
+
+            Console.WriteLine($"Number of letters in the sentence: {lettercount}");
+        }
+    }
+}
